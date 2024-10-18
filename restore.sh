a2a54cc463d3e6b13e3ef466478a92a5a03a4fa6
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-10-18 11:10:27 +0300

    restore more simply

--- restore.sh
+++ restore.sh
@@ -1,5 +1,2 @@
-sudo ln -sf "$PWD"/flake.nix /etc/nixos
-sudo cp /etc/nixos/hardware-configuration.nix "$PWD/"
-sudo mv /etc/nixos/ /tmp/moved_nixos
-sudo ln -sf "$PWD"/ /etc/nixos
 sudo nixos-generate-config
+sudo cp /etc/nixos/hardware-configuration.nix "$PWD/"
