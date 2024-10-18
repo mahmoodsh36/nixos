27b0cfadb4228018a00d6da82c78e1c3b069a384
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-10-18 11:05:39 +0300

    add keyd

--- desktop.nix
+++ desktop.nix
@@ -520,5 +520,34 @@ in
     };
   };
 
+  # for keyd
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        ids = [ "*" ]; # what goes into the [id] section, here we select all keyboards
+          # everything but the ID section:
+          settings = {
+            # the main layer, if you choose to declare it in Nix
+            main = {
+              capslock = "layer(control)"; # you might need to also enclose the key in quotes if it contains non-alphabetical symbols
+            };
+            otherlayer = {};
+          };
+        extraConfig = ''
+            # put here any extra-config, e.g. you can copy/paste here directly a configuration, just remove the ids part
+          '';
+      };
+    };
+  };
+  # optional, but makes sure that when you type the make palm rejection work with keyd
+  # https://github.com/rvaiya/keyd/issues/723
+  environment.etc."libinput/local-overrides.quirks".text = ''
+    [Serial Keyboards]
+    MatchUdevType=keyboard
+    MatchName=keyd virtual keyboard
+    AttrKeyboardIntegration=internal
+  '';
+
   system.stateVersion = "24.05"; # dont change
 }
