789f6b9b00921e90f696b3f190d10c184476198b
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-11-19 09:40:26 +0200

    remove authorized_keys home file

--- home.nix
+++ home.nix
@@ -127,8 +127,4 @@ in
       package = pkgs.adwaita-qt;
     };
   };
-
-  home.file.".ssh/authorized_keys" = { text = ''
-    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICQaNODbg0EX196+JkADTx/cB0arDn6FelMGsa0tD0p6 mahmooz@mahmooz
-  ''; };
 }
