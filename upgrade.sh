55ff3686a737e26d61d9e4bd3b0f6d7da2e31f97
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-11-30 11:53:27 +0200

    small changes

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 cd ~/work/nixos/
 cp /etc/nixos/hardware-configuration.nix .
-sudo nixos-rebuild switch --upgrade --flake .#mahmooz --option eval-cache false --show-trace "$@"
+sudo nixos-rebuild switch --upgrade --flake .#mahmooz --option eval-cache false --show-trace --impure "$@"
