2aa27564e6af0c63d01327ab5bc9ff58031ea85a
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-12-27 12:39:02 +0200

    use greetd

--- desktop.nix
+++ desktop.nix
@@ -158,10 +158,10 @@ in
     # };
   };
   services.displayManager = {
-    autoLogin = {
-      enable = true;
-      user = "mahmooz";
-    };
+    # autoLogin = {
+    #   enable = true;
+    #   user = "mahmooz";
+    # };
     # defaultSession = "none+awesome";
     # defaultSession = "xfce+awesome";
     # defaultSession = "xfce";
@@ -595,6 +595,32 @@ in
     enable = true;
     platformTheme = "qt5ct";
     style = "adwaita-dark";
+  };
+
+  # https://github.com/sjcobb2022/nixos-config/blob/29077cee1fc82c5296908f0594e28276dacbe0b0/hosts/common/optional/greetd.nix
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+        user = "mahmooz";
+      };
+    };
+  };
+
+  # this is a life saver.
+  # literally no documentation about this anywhere.
+  # might be good to write about this...
+  # https://www.reddit.com/r/NixOS/comments/u0cdpi/tuigreet_with_xmonad_how/
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
   };
 
   system.stateVersion = "24.05"; # dont change
