a35a08721691c279accc58efb69bbe0520ed067c
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-01-18 22:32:43 +0200

    reenable xwayland on hyprland, update flake

--- desktop.nix
+++ desktop.nix
@@ -181,6 +181,7 @@ in
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    xwayland.enable = true;
   };
 
   # tty configs
