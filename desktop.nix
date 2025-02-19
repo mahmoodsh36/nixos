cbaf0b05044b128f3f60ee5253a07540cf35e8d0
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-02-19 14:57:12 +0200

    use tlp, configure a few boot kernel params

--- desktop.nix
+++ desktop.nix
@@ -11,6 +11,46 @@ in
     ./server.nix
   ] ++ lib.optional (per_machine_vars.enable_nvidia) ./nvidia.nix;
 
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "boot.shell_on_fail"
+  ];
+
+  # tlp for battery saving?
+  services.tlp = {
+    enable = true;
+    settings = {
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "balanced";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_BOOST_ON_AC=1;
+      CPU_BOOST_ON_BAT=0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_BAT = 30;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_DRIVER_OPMODE_ON_AC = "active";
+      CPU_DRIVER_OPMODE_ON_BAT = "active";
+      STOP_CHARGE_THRESH_BAT1 = 80;
+      USB_AUTOSUSPEND = 0;
+      USB_BLACKLIST_WWAN = 1;
+      WIFI_PWR_ON_AC = "off";
+      WIFI_PWR_ON_BAT = "off";
+      RESTORE_DEVICE_STATE_ON_STARTUP = 0;
+      USB_ALLOWLIST = "46d:c07e";
+      USB_EXCLUDE_BTUSB = 1;
+      USB_EXCLUDE_PHONE = 1;
+      RUNTIME_PM_BLACKLIST="0bda:8153"; # for ethernet port
+    };
+    # extraConfig = ''
+    #   USB_AUTOSUSPEND=0
+    # '';
+  };
+
   # better safe than sorry (for having to deal with firmware/driver issues)..?
   hardware.enableAllHardware = true;
   hardware.enableAllFirmware = true;
