1cef8f6535483a16184a6212dbd03d9f9611f704
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-04-01 22:10:01 +0300

    update pinned-pkgs

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     # yet another pinning mechanism (beside the fact that this is a flake..)
-    pinned-pkgs.url = "github:NixOS/nixpkgs/5e5402ecbcb27af32284d4a62553c019a3a49ea6";
+    pinned-pkgs.url = "github:NixOS/nixpkgs/52faf482a3889b7619003c0daec593a1912fddc1";
     # pinned-pkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
