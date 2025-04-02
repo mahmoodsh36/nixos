fc4ab7945079e3fff0ebe8f98346a02ef2ecb7e0
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-04-02 19:06:17 +0300

    add mistral-rs

--- desktop.nix
+++ desktop.nix
@@ -480,7 +480,7 @@ in
   ]
   ++ server_vars.server_packages
   ++ (pkgs.lib.optionals per_machine_vars.enable_nvidia [
-    cudatoolkit nvtopPackages.full llama-cpp
+    cudatoolkit nvtopPackages.full llama-cpp mistral-rs
   ]);
 
   systemd.services.my_mpv_logger_service = {
