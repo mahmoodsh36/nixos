9b1f444ac4806e3b98a19c6db6d783d995b41599
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-04-08 14:51:34 +0300

    add --refresh for reference

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 cd ~/work/nixos/
 cp /etc/nixos/hardware-configuration.nix .
-# sudo nixos-rebuild switch --upgrade --flake .#mahmooz --option eval-cache false --show-trace --impure "$@"
+# sudo nixos-rebuild switch --upgrade --flake .#mahmooz --option eval-cache false --refresh --show-trace --impure "$@"
 sudo nixos-rebuild switch --upgrade --flake .#mahmooz --show-trace --impure "$@"
