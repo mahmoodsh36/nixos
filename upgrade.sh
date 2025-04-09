50cda550bc2301f623a14139a41a0df70c4fe576
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-04-09 14:19:39 +0300

    fix issue in script

--- upgrade.sh
+++ upgrade.sh
@@ -5,4 +5,4 @@ echo building for $conf
 cd ~/work/nixos/
 cp /etc/nixos/hardware-configuration.nix .
 # sudo nixos-rebuild switch --upgrade --flake .#mahmooz --option eval-cache false --refresh --show-trace --impure "$@"
-sudo nixos-rebuild switch --upgrade --flake .#"$conf" --show-trace --impure "$@"
+sudo nixos-rebuild switch --upgrade --flake .#"$conf" --show-trace --impure
