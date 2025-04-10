5fa6f88b11ba22a98d537264a7d25459cba1a9d2
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-04-10 22:52:58 +0300

    add more cachix instances

--- server.nix
+++ server.nix
@@ -185,10 +185,13 @@ in
   # for binaries of nonfree packages, like pytorch (otherwise nix will try to compile them)
   nix.settings.substituters = [
     "https://nix-community.cachix.org"
+    "https://cache.nixos.org/"
+    "https://text-generation-inference.cachix.org"
   ];
   nix.settings.trusted-public-keys = [
     # compare to the key published at https://nix-community.org/cache
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "text-generation-inference.cachix.org-1:xdQ8eIf9LuIwS0n0/5ZmOHLaCXC6yy7MgzQNK/y+R1c="
   ];
 
   environment.systemPackages = server_vars.server_packages;
