7d65ae1e6de5716072aa2ea50defd5ffa67180b7
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-04-11 16:56:31 +0300

    fix color escape sequence in script

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 conf="$1"
 [ -z "$conf" ] && conf="mahmooz1"
-echo building for \e[0;31m$conf\e[0m
+echo -e building for "\e[31m$conf\e[0m"
 cd ~/work/nixos/
 cp /etc/nixos/hardware-configuration.nix .
 # sudo nixos-rebuild switch --upgrade --flake .#mahmooz --option eval-cache false --refresh --show-trace --impure "$@"
