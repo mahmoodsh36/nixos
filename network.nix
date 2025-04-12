b27e4ccdddbf78feb0e31c48ac81811f4da2ca20
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-04-12 13:59:11 +0300

    more work on vpn config

--- network.nix
+++ network.nix
@@ -3,6 +3,11 @@
 let
   server_vars = (import ./server_vars.nix { pkgs = pkgs; pinned-pkgs = pinned-pkgs; });
   constants = (import ./constants.nix);
+  private_domain = "mahmooz3.lan";
+  headscale_host = "headscale.${private_domain}";
+  grafana_host = "grafana.${private_domain}";
+  grafana_port = 3000;
+  headscale_port = 8443;
 in rec
 {
   services.openssh = {
@@ -46,43 +51,61 @@ in rec
     authKeyFile = "${constants.home_dir}/brain/keys/headscale1";
   };
 
+
   services.headscale = {
     enable = true;
     address = "0.0.0.0";
-    port = 8443;
-    serverUrl = "https://${constants.mahmooz3_addr}:8443";
+    # port = 8443;
     settings = {
+      server_url = "https://${headscale_host}:${toString headscale_port}";
       dns = {
-        magic_dns = false;
+        # override_local_dns = true;
+        base_domain = private_domain;
+        magic_dns = true;
+        # domains = [ headscale_host ];
+        nameservers.global = [
+          "1.1.1.1" # cloudflare
+          "9.9.9.9" # quad9
+        ];
       };
     };
   };
 
   services.caddy = {
     enable = true;
-    # reverse proxy traffic to headscale
-    configFile = pkgs.writeText "caddyfile" ''
-      https://${constants.mahmooz3_addr}/headscale {
+    # configure some reverse proxy traffic
+    # allow connection via public ip?
+    virtualHosts."grafana.${constants.mahmooz3_addr}" = {
+       extraConfig = ''
+         redir https://${grafana_host}{uri} permanent
+       '';
+    };
+    # for machines that are part of the tailnet
+    virtualHosts."${headscale_host}" = {
+      extraConfig = ''
         tls internal
-        reverse_proxy localhost:8443
-      }
-      https://${constants.mahmooz3_addr}/grafana {
+        reverse_proxy localhost:${toString headscale_port}
+      '';
+    };
+    virtualHosts."${grafana_host}" = {
+      extraConfig = ''
         tls internal
-        reverse_proxy localhost:3000
-      }
-      http://${constants.mahmooz3_addr} {
-        redir https://${constants.mahmooz3_addr}{uri}
-      }
-    '';
+        reverse_proxy localhost:${toString grafana_port}
+      '';
+    };
   };
   # allow the caddy user(and service) to edit certs
   services.tailscale.permitCertUid = "caddy";
 
   networking.firewall = {
-    allowedTCPPorts = [ 22 80 443 2222 ];
+    allowedTCPPorts = [
+      22 2222 # ssh
+      80 # caddy - http
+      443 # caddy - https
+    ];
     enable = true;
     allowedUDPPorts = [ services.tailscale.port ];
-    trustedInterfaces = [ "tailscale0" ];
+    trustedInterfaces = [ config.services.tailscale.interfaceName ];
     checkReversePath = "loose"; # https://github.com/tailscale/tailscale/issues/4432#issuecomment-1112819111
   };
 
@@ -91,9 +114,9 @@ in rec
     settings = {
       server = {
         http_addr = "0.0.0.0";
-        http_port = 3000;
-        domain = "https://${constants.mahmooz3_addr}";
-        root_url = "https://${constants.mahmooz3_addr}/grafana/";
+        http_port = grafana_port;
+        domain = private_domain;
+        root_url = grafana_host;
         serve_from_sub_path = true;
       };
     };
