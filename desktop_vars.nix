caf38d9d1d91c1680b9381606d1c95de9931b012
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-04-17 21:54:30 +0300

    add gguf to python env

--- desktop_vars.nix
+++ desktop_vars.nix
@@ -1,5 +1,13 @@
 { pkgs, config, pinned-pkgs, ... }:
 
+let
+  ggufrepo = pkgs.fetchFromGitHub {
+    owner = "ggerganov";
+    repo = "llama.cpp";
+    rev = "92bc493917d43b83e592349e138b54c90b1c3ea7";
+    sha256 = "03k3rqc2g74h2nb0lx7vg6jw5fdyc9l0dqfi5jlbcn1dhxj1lagk";
+  };
+in
 {
   desktop_python = (pinned-pkgs.python3.withPackages (ps: with ps; [
     matplotlib flask requests numpy sympy networkx pydot
@@ -11,8 +19,15 @@
     # pyzmq
   ] ++ pkgs.lib.optionals config.machine.enable_nvidia [
     # machine learning
-    torchWithCuda # accelerate
+    torchWithCuda
     transformers datasets
+    (gguf.overrideAttrs {
+      src = "${ggufrepo}/gguf-py" ;
+      doCheck = false;
+      doInstallCheck = false;
+      dontCheck = true;
+      dontCheckRuntimeDeps=true;
+    })
   ]));
   desktop_julia = (pinned-pkgs.julia.withPackages.override({
     precompile = true;
