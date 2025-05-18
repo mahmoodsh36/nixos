ca083fcf16472dad0caa8300f514274b12d368a7
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-05-18 21:13:29 +0300

    correctly use "ref" instead of "rev"

--- home.nix
+++ home.nix
@@ -8,14 +8,14 @@ let
          then "${config.home.homeDirectory}/work/otherdots"
          else (builtins.fetchGit {
            url = "${constants.mygithub}/otherdots.git";
-           rev = "master";
+           ref = "master";
          }).outPath;
 
   nvim_dots = if builtins.pathExists "${config.home.homeDirectory}/work/nvim"
          then "${config.home.homeDirectory}/work/nvim"
          else (builtins.fetchGit {
            url = "${constants.mygithub}/nvim.git";
-           rev = "master";
+           ref = "master";
          }).outPath;
 
   config_names = [
@@ -34,7 +34,7 @@ let
          then "${config.home.homeDirectory}/work/scripts"
          else (builtins.fetchGit {
            url = "${constants.mygithub}/scripts.git";
-           rev = "master";
+           ref = "master";
          }).outPath;
   scripts = builtins.attrNames (builtins.readDir scripts_dir);
   script_files = builtins.filter (name: builtins.match ".*\\.(py|sh|el)$" name != null) scripts;
