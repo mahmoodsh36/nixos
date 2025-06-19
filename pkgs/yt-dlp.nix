74783875152064762606c312d96a022baf511aa7
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-06-19 22:32:28 +0300

    patch yt-dlp unsupported.py

--- pkgs/yt-dlp.nix
+++ pkgs/yt-dlp.nix
@@ -1,13 +1,10 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ yt-dlp, perl }:
 
-pkgs.yt-dlp.overrideAttrs (old: {
-  nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.perl ];
+yt-dlp.overrideAttrs (old: {
+  nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ perl ];
+
   patchPhase = ''
-    echo doing here ==
-    cat yt_dlp/extractor/unsupported.py
-    perl -0777 -i -pe 's/^[ \t]*URLS = \((?!\))(.|\n)*?^[ \t]*\)/    URLS = ()\n/msg' yt_dlp/extractor/unsupported.py
-    cat yt_dlp/extractor/unsupported.py
-    echo doing here2 ==
     ${old.patchPhase or ""}
+    perl -0777 -i -pe 's/^[ \t]*URLS = \((?!\))(.|\n)*?^[ \t]*\)/    URLS = ()\n/msg' yt_dlp/extractor/unsupported.py
   '';
 })
