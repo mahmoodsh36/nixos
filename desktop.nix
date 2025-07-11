b0ede547a7acc3e1988e3ab2b8b923c86f936ca2
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-07-11 19:45:56 +0300

    add yt-dlp overlay with special patch

--- desktop.nix
+++ desktop.nix
@@ -109,6 +109,25 @@ in
             });
         };
       })
+      (final: prev: {
+        yt-dlp = prev.yt-dlp.overrideAttrs (old: {
+          nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.perl ];
+
+          # src = pkgs.fetchFromGitHub {
+          #   owner = "yt-dlp";
+          #   repo = "yt-dlp";
+          #   rev = "73bf10211668e4a59ccafd790e06ee82d9fea9ea";
+          #   sha256 = "07kkrmbld6jsknyyf3b171njdmh73xfjf86k6fl5zd30bma1fbiw";
+          # };
+
+          # to remove the blocked urls
+          patchPhase = ''
+    ${old.patchPhase or ""}
+    perl -0777 -i -pe 's/^[ \t]*URLS = \((?!\))(.|\n)*?^[ \t]*\)/    URLS = ()\n/msg' yt_dlp/extractor/unsupported.py
+    perl -0777 -i -pe 's/^([ \t]*)_TESTS = \[\{(?:.|\n)*?\}\][ \t]*\n/''${1}_TESTS = []\n/msg' yt_dlp/extractor/unsupported.py
+  '';
+        });
+      })
 
       inputs.mcp-servers-nix.overlays.default
     ] ++ server_vars.server_overlays;
@@ -438,7 +457,7 @@ in
       spotdl
       openjdk
       transmission_4 acpi lm_sensors
-      you-get aria ytdl-sub (import ./pkgs/yt-dlp.nix { pkgs = pkgs; })
+      you-get aria ytdl-sub yt-dlp
       playwright
       uv
       argc
