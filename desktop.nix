a7f654bd3262776ea3b70449b8fccc75335e9f02
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-07-17 14:48:26 +0300

    update flake, disable lem

--- desktop.nix
+++ desktop.nix
@@ -370,7 +370,7 @@ in
       #   exec ${main_julia}/bin/julia "$@"
       # '')
 
-      inputs.lem.packages.${pkgs.system}.lem-sdl2
+      # inputs.lem.packages.${pkgs.system}.lem-sdl2
       code-cursor
       neovide
       windsurf
