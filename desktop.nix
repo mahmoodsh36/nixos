8a1ca33f1ded8e0f248e8e225d4add8208ebdd5f
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-08-20 23:59:50 +0300

    remove unused yt-dlp patch

--- desktop.nix
+++ desktop.nix
@@ -112,27 +112,6 @@ in
 
     # my overlays
     nixpkgs.overlays = [
-      # the patch now messes things up
-  #     (final: prev: {
-  #       yt-dlp = prev.yt-dlp.overrideAttrs (old: {
-  #         nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.perl ];
-
-  #         # src = pkgs.fetchFromGitHub {
-  #         #   owner = "yt-dlp";
-  #         #   repo = "yt-dlp";
-  #         #   rev = "73bf10211668e4a59ccafd790e06ee82d9fea9ea";
-  #         #   sha256 = "07kkrmbld6jsknyyf3b171njdmh73xfjf86k6fl5zd30bma1fbiw";
-  #         # };
-
-  #         # to remove the blocked urls
-  #         patchPhase = ''
-  #   ${old.patchPhase or ""}
-  #   perl -0777 -i -pe 's/^[ \t]*URLS = \((?!\))(.|\n)*?^[ \t]*\)/    URLS = ()\n/msg' yt_dlp/extractor/unsupported.py
-  #   perl -0777 -i -pe 's/^([ \t]*)_TESTS = \[\{(?:.|\n)*?\}\][ \t]*\n/''${1}_TESTS = []\n/msg' yt_dlp/extractor/unsupported.py
-  # '';
-  #       });
-  #     })
-
       inputs.mcp-servers-nix.overlays.default
       inputs.stable-diffusion-webui-nix.overlays.default
     ] ++ server_vars.server_overlays;
