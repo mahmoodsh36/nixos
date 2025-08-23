1239b537663025ce67a83aa2fdf051021bb7e539
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-08-23 17:12:11 +0300

    slight change

--- llm.nix
+++ llm.nix
@@ -128,7 +128,7 @@ in
             --temp 1.1 --min-p 0.0 --top-p 0.95 --top-k 20 --presence-penalty 1.4\
             --no-kv-offload --port 5000 --host 0.0.0.0 --seed 2\
             --cache-type-k q8_0 --cache-type-v q8_0\
-            --chat-template-kwargs '{"thinking_budget": 8000}'
+            --chat-template-kwargs '{"thinking_budget": 1024}'
         '';
         # script = ''
         #   ${config.machine.llama-cpp.pkg}/bin/llama-server\
