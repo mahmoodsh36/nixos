efc9005227784a9d4ee70ed37db67ab3a445198e
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-08-23 16:24:34 +0300

    update flake, build llama-cpp from master

--- machine-config.nix
+++ machine-config.nix
@@ -25,13 +25,6 @@ in
     machine.llama-cpp.pkg =
       (if config.machine.enable_nvidia
        then inputs.llama-cpp-flake.packages.${pkgs.system}.cuda
-       else inputs.llama-cpp-flake.packages.${pkgs.system}.default).overrideAttrs (_: {
-         src = pkgs.fetchFromGitHub {
-           owner = "pwilkin";
-           repo = "llama.cpp";
-           rev = "f4988204c3bb4dec1e9fb52bf876c178f989f9b7";
-           sha256 = "sha256-J8IxayW8Gn/t0+YMmInV23cBE2qC+v51gTAlEEiHl4Q=";
-         };
-       });
+       else inputs.llama-cpp-flake.packages.${pkgs.system}.default);
   };
 }
