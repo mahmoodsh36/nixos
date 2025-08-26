9f77df44607ed763dffcfb11c5fe323984734893
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-08-26 20:26:23 +0300

    slight change

--- desktop.nix
+++ desktop.nix
@@ -415,7 +415,7 @@ in
       wl-screenrec
       libnotify
       # darktable # image editor
-      digikam # another image viewer?
+      # digikam # another image viewer?
       swww # wallpaper setter
 
       vdhcoapp # for firefox video download helper
