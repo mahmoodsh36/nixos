a053e0aa5019739ee5c6acbbcb8b7ef7d6bfc426
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-08-30 22:44:50 +0300

    slight change

--- llm.nix
+++ llm.nix
@@ -124,7 +124,7 @@ in
         script = ''
           ${config.machine.llama-cpp.pkg}/bin/llama-server\
             -hf unsloth/Qwen3-4B-Thinking-2507-GGUF:Q8_0\
-            --jinja -ngl 99 --threads 16 --ctx-size 200000 -fa\
+            --jinja -ngl 99 --threads 16 --ctx-size 200000 -fa on\
             --temp 0.6 --min-p 0.0 --top-p 0.95 --top-k 20 --presence-penalty 1.4\
             --port 5000 --host 0.0.0.0 --seed 2 --cache-type-k q8_0 --cache-type-v q8_0
         '';
