18ec7c4ae6964edf9f3305ac24a389874465bc91
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-09-21 03:23:44 +0300

    unpin sage

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -459,12 +459,12 @@ in
       # some programming languages/environments
       (texlive.combined.scheme-full.withPackages((ps: with ps; [ pkgs-pinned.sagetex ])))
       typst
-      # (lib.mkIf (!config.machine.enable_nvidia) pkgs-pinned.sageWithDoc) # to avoid building
-      (lib.mkIf (!config.machine.enable_nvidia)
-        (myutils.packageFromCommit {
-          rev = "c2ae88e026f9525daf89587f3cbee584b92b6134b9";
-          packageName = "sageWithDoc";
-        }))
+      (lib.mkIf (!config.machine.enable_nvidia) pkgs-pinned.sageWithDoc) # to avoid building
+      # (lib.mkIf (!config.machine.enable_nvidia)
+      #   (myutils.packageFromCommit {
+      #     rev = "c2ae88e026f9525daf89587f3cbee584b92b6134b9";
+      #     packageName = "sageWithDoc";
+      #   }))
 
       # lsp
       nodePackages.bash-language-server
