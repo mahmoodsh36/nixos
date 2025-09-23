f975a4bd22e366547a1375af26d1ddadd2efaf1e
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-09-23 15:19:43 +0300

    enable ccache (for robotnix)

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -106,6 +106,33 @@ in
     nixpkgs.overlays = [
       inputs.mcp-servers-nix.overlays.default
       inputs.stable-diffusion-webui-nix.overlays.default
+      # needed for robotnix
+      (self: super: {
+        ccacheWrapper = super.ccacheWrapper.override {
+          extraConfig = ''
+            export CCACHE_COMPRESS=1
+            export CCACHE_DIR="${config.programs.ccache.cacheDir}"
+            export CCACHE_UMASK=007
+            export CCACHE_SLOPPINESS=random_seed
+            if [ ! -d "$CCACHE_DIR" ]; then
+              echo "====="
+              echo "Directory '$CCACHE_DIR' does not exist"
+              echo "Please create it with:"
+              echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
+              echo "  sudo chown root:nixbld '$CCACHE_DIR'"
+              echo "====="
+              exit 1
+            fi
+            if [ ! -w "$CCACHE_DIR" ]; then
+              echo "====="
+              echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
+              echo "Please verify its access permissions"
+              echo "====="
+              exit 1
+            fi
+          '';
+        };
+      })
     ];
 
     # graphical stuff (wayland,x11,etc)
@@ -690,5 +717,9 @@ in
         };
       };
     };
+
+    # ccache is needed for robotnix
+    nix.settings.extra-sandbox-paths = [ config.programs.ccache.cacheDir ];
+    programs.ccache.enable = true;
   };
 }
