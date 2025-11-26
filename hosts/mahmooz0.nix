998afeb0af5394b099e5a74374a72840d5d3eae1
mahmoodsh36 <mahmod.m2015@gmail.com>
2025-11-27 00:40:38 +0200

    enable linux builder (external builders etc)

--- hosts/mahmooz0.nix
+++ hosts/mahmooz0.nix
@@ -162,7 +162,26 @@ in
     #   });
     # };
 
-    # other configuration parameters
-    # see here: https://nix-darwin.github.io/nix-darwin/manual
+    # let determinate nix handle your nix configuration
+    nix.enable = false;
+
+    # custom determinate nix settings written to /etc/nix/nix.custom.conf
+    determinate-nix.customSettings = {
+      # enables parallel evaluation (remove this setting or set the value to 1 to disable)
+      eval-cores = 0;
+      extra-experimental-features = [
+        "build-time-fetch-tree" # enables build-time flake inputs
+        "parallel-eval" # enables parallel evaluation
+        "external-builders"
+      ];
+      external-builders = builtins.toJSON [{
+        systems = ["aarch64-linux" "x86_64-linux"];
+        program = "/usr/local/bin/determinate-nixd";
+        args = ["builder"];
+      }];
+      extra-trusted-users = ["${config.machine.user}" "@admin" "@root" "@sudo" "@wheel" "@staff"];
+      keep-outputs = true;
+      keep-derivations = true;
+    };
   };
 }
