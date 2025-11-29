a5259479913d1c9919fb2434c6b2d9de4214165f
mahmoodsh36 <mahmod.m2015@gmail.com>
2025-11-29 19:42:53 +0200

    overrideLocalDns for mac

--- profiles/home/home.nix
+++ profiles/home/home.nix
@@ -388,7 +388,7 @@ in
                 "--entrypoint=" # Clear the broken ENTRYPOINT from base image
                 "--device" "/dev/dri"
                 "--memory" "32g"
-                "-v" "${config'.machine.voldir}/models:/app/models"
+                # "-v" "${config'.machine.voldir}/models:/app/models"
                 "-e" "HF_HOME=/app/models"
                 "-e" "TRANSFORMERS_CACHE=/app/models"
                 "-e" "HUGGINGFACE_HUB_CACHE=/app/models"
