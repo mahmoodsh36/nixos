19677a100e0e9756950e1c7a04c96420ee799e8b
mahmoodsh36 <mahmod.m2015@gmail.com>
2025-12-10 01:56:16 +0200

    option to build offline

--- upgrade.sh
+++ upgrade.sh
@@ -13,12 +13,18 @@ cd ~/work/nixos/
 # sudo nixos-rebuild switch --upgrade --flake .#mahmooz --option eval-cache false --refresh --show-trace --impure "$@"
 # sudo nixos-rebuild switch --upgrade --flake .#mahmooz --show-trace --impure --option eval-cache false --refresh
 # EXPORT MAX_JOBS=6; sudo -E nixos-rebuild switch --upgrade --flake .#mahmooz2 --option cores 6 --option max-jobs 6 --impure
+extra_args=""
+if [ "$2" == "offline" ]; then
+    echo "Running in offline mode..."
+    extra_args="--offline"
+fi
+
 if [ "$conf" == "droid" ]; then
-    nix-on-droid switch --flake .#"$conf" --show-trace
+    nix-on-droid switch --flake .#"$conf" --show-trace $extra_args
 elif [ "$conf" == "mahmooz0" ]; then
-    sudo darwin-rebuild switch --flake .#mahmooz0 --show-trace --impure
+    sudo darwin-rebuild switch --flake .#mahmooz0 --show-trace --impure $extra_args
 else
     # sudo -E nixos-rebuild switch --upgrade --flake .#"$conf" --show-trace --impure --install-bootloader
     # the -E option is sometimes problematic, but nixos-rebuild has to be run as root
-    sudo -E nixos-rebuild switch --flake .#"$conf" --show-trace --impure --install-bootloader
+    sudo -E nixos-rebuild switch --flake .#"$conf" --show-trace --impure --install-bootloader $extra_args
 fi
