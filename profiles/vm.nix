471316e11c330fb01bf3004d7c19c038428ae20d
mahmoodsh36 <mahmod.m2015@gmail.com>
2026-01-03 01:32:57 +0200

    add voldir as shared dir for vm

--- profiles/vm.nix
+++ profiles/vm.nix
@@ -8,6 +8,12 @@
     cores = 8;
     diskSize = 80 * 1024;
     fileSystems."/".autoResize = true;
+
+    fileSystems."/mnt/host_share" = {
+      fsType = "9p";
+      device = "host_share";
+      options = [ "trans=virtio" "version=9p2000.L" ];
+    };
     graphics = config.machine.is_desktop;
     diskImage = "\${NIX_DISK_IMAGE:-${hostVoldir}/vm/mahmooz1${if config.machine.is_desktop then "" else "-headless"}.qcow2}";
     resolution = { x = 1280; y = 720; };
@@ -18,6 +24,11 @@
       # use mkForce to override the default virtio networking from qemu-vm.nix module
       # which causes TX timeout issues on macOS hosts
       networkingOptions = lib.mkForce [
+      ];
+
+      options = [
+        "-fsdev" "local,id=fsdev0,path=${hostVoldir},security_model=none"
+        "-device" "virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=host_share"
       ];
     };
   };
