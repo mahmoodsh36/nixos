5562d169204046bea2ed97874013b8467550a544
mahmoodsh36 <mahmod.m2015@gmail.com>
2026-01-09 23:38:03 +0200

    disable plasma

--- lib/constants.nix
+++ lib/constants.nix
@@ -8,7 +8,7 @@ rec {
   mygithub = "https://github.com/mahmoodsh36";
   personal_website = "https://mahmoodsh36.github.io";
   ssh_pub_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICQaNODbg0EX196+JkADTx/cB0arDn6FelMGsa0tD0p6 mahmooz@mahmooz";
-  enable_plasma = true;
+  enable_plasma = false;
   enable_gnome = true;
   password = "mahmooz";
 }
--- upgrade.sh
+++ upgrade.sh
@@ -25,7 +25,7 @@ fi
 if [ "$conf" == "droid" ]; then
     nix-on-droid switch --flake .#"$conf" --show-trace $extra_args
 elif [ "$conf" == "mahmooz0" ]; then
-    sudo darwin-rebuild switch --flake .#mahmooz0 --show-trace --impure $extra_args
+    sudo darwin-rebuild switch --flake .#mahmooz0 --show-trace --impure $extra_args --keep-going
 else
     # sudo -E nixos-rebuild switch --upgrade --flake .#"$conf" --show-trace --impure --install-bootloader
     # the -E option is sometimes problematic, but nixos-rebuild has to be run as root
