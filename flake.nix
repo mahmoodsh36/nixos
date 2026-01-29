e78369ff80a9361f67b8c32d7897224039767d10
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2026-01-29 03:04:34 +0200

    update flake, pin nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "nixos flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/88d3861acdd3d2f0e361767018218e51810df8a1";
     pkgs-pinned.url = "github:NixOS/nixpkgs/c0b0e0fddf73fd517c3471e546c0df87a42d53f4";
     pkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
