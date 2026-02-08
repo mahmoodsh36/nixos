7a3cc2242571e5a32e33d55a913451b14d0c904b
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2026-02-08 22:46:16 +0200

    slight change

--- profiles/server.nix
+++ profiles/server.nix
@@ -71,7 +71,7 @@ in
       devenv
       podman-compose
       sbcl.pkgs.qlot-cli
-      yt-dlp (lib.mkIf (!config.machine.is_vm) ytdl-sub)
+      pkgs-unstable.yt-dlp (lib.mkIf (!config.machine.is_vm) ytdl-sub)
       # (yt-dlp.overrideAttrs (finalAttrs: prevAttrs: {
       #   src = pkgs.fetchFromGitHub {
       #     owner = "yt-dlp";
