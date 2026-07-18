3a7d634ff23ba35d9beaa7c3b540e96fbea495da
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2026-07-18 19:30:17 +0300

    small change

--- profiles/home/karabiner.nix
+++ profiles/home/karabiner.nix
@@ -74,7 +74,7 @@ in
                   (swapKey "right_command" "left_control")
                   (launchApp "b" "Firefox")
                   (launchApp "e" "Emacs")
-                  (launchApp "u" "Youtube Music")
+                  (launchApp "u" "Spotify")
                   (launchApp "m" "Lem")
                   # (launchApp "return_or_enter" "WezTerm")
                   (launchCommand "return_or_enter" "export WEZTERM_CONFIG_FILE=$HOME/.config/wezterm/wezterm.lua; open -a wezterm")
